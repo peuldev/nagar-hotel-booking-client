b8b091d35ac7cbba4e95ebfb13556d3cbeb8ac6e
peuldev <arfinpeul523@gmail.com>
2023-12-22 17:32:17 +0600

    responsive location checking

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -6,12 +6,14 @@ const Banner = () => {
     <div className="max-w-7xl mx-auto">
       <div className="mx-2">
         <div>
-          <h1 className="text-7xl font-medium ">Welcome to Nagar</h1>
+          <h1 className="lg:text-7xl md:text-3xl font-medium ">
+            Welcome to Nagar
+          </h1>
           <p>Deleniti nostrum laboriosam praesentium quasi quam voluptate.</p>
         </div>
-        <div className="bg-white p-2 my-20 rounded-lg">
+        <div className="bg-white p-2 my-20 rounded-lg grid lg:grid-flow-col md:grid-cols-2 gap-5 grid-flow-row">
           <div className="join">
-            <button className="mx-3 text-base text-[#64688c]">Check-in</button>
+            <button className="mr-3 text-base text-[#64688c]">Check-in</button>
             <input
               className="input bg-[#ecfafb] rounded-lg"
               placeholder="Date"
@@ -19,16 +21,16 @@ const Banner = () => {
             />
           </div>
           <div className="join">
-            <button className="mx-3 text-base text-[#64688c]">Check-Out</button>
+            <button className="mr-3 text-base text-[#64688c]">Check-Out</button>
             <input
-              className="input bg-[#ecfafb] rounded-lg"
+              className="input  bg-[#ecfafb] rounded-lg"
               placeholder="Date"
               type="date"
             />
           </div>
 
           <div className="join">
-            <button className="mx-3 text-base text-[#64688c]">Adults</button>
+            <button className="mr-3 text-base text-[#64688c]">Adults</button>
             {/* <input className="input join-item" placeholder="Date" type="date" /> */}
             <select className="select w-full max-w-xs bg-[#ecfafb] rounded-lg">
               <option disabled selected>
@@ -42,7 +44,7 @@ const Banner = () => {
             </select>
           </div>
           <div className="join">
-            <button className="mx-3 text-base text-[#64688c]">Children</button>
+            <button className="mr-3 text-base text-[#64688c]">Children</button>
             {/* <input className="input join-item" placeholder="Date" type="date" /> */}
             <select className="select w-full max-w-xs bg-[#ecfafb] rounded-lg">
               <option disabled selected>
@@ -56,8 +58,8 @@ const Banner = () => {
             </select>
           </div>
           <div className="join">
-            <button className="mx-3 text-base text-[#64688c]">Find</button>{" "}
-            <p className=" bg-blue hover:bg-info text-white rounded-full cursor-pointer text-xl px-2 py-2">
+            <button className="mr-3 text-base text-[#64688c]">Find</button>{" "}
+            <p className=" bg-blue hover:bg-info text-white rounded-full cursor-pointer text-xl p-3">
               {" "}
               <IoIosSearch />
             </p>{" "}
