3e412f58b0d6fbe87fb337af250d0ec222614672
peuldev <arfinpeul523@gmail.com>
2023-12-23 21:38:21 +0600

    Facilities update

--- src/components/FeaturedRooms/DetailsFeaturedRoom.jsx
+++ src/components/FeaturedRooms/DetailsFeaturedRoom.jsx
@@ -1,18 +1,6 @@
 import React from "react";
 import { useLoaderData } from "react-router-dom";
-import {
-  FaBed,
-  FaRulerCombined,
-  FaBinoculars,
-  FaWifi,
-  FaCocktail,
-  FaSwimmingPool,
-  FaChild,
-  FaTableTennis,
-  FaBath,
-  FaWheelchair,
-} from "react-icons/fa";
-import { MdHotTub } from "react-icons/md";
+import { FaBed, FaRulerCombined, FaBinoculars } from "react-icons/fa";
 
 const DetailsFeaturedRoom = () => {
   const featuredRooms = useLoaderData();
@@ -59,53 +47,26 @@ const DetailsFeaturedRoom = () => {
           </h1>
         </div>
         {/* table use  */}
-        <div className="max-w-2xl mx-auto">
+        <div className="max-w-2xl mx-auto pb-20">
           <div className="overflow-x-auto">
             <table className="table table-zebra">
               <tbody>
                 {/* row 1 */}
                 <tr>
-                  <td>
-                    {" "}
-                    <FaWifi /> High speed in-room wifi
-                  </td>
-                  <td>
-                    <FaCocktail /> Mini bar
-                  </td>
+                  <td>High speed in-room wifi</td>
+                  <td>Mini bar</td>
                 </tr>
-                {/* row 2 */}
                 <tr>
-                  <td>
-                    {" "}
-                    <FaSwimmingPool />
-                    Swimming pool
-                  </td>
-                  <td>
-                    {" "}
-                    <FaChild /> Child friendly
-                  </td>
+                  <td>Swimming pool</td>
+                  <td>Child friendly</td>
                 </tr>
-                {/* row 3 */}
                 <tr>
-                  <td>
-                    {" "}
-                    <MdHotTub /> Hot tub
-                  </td>
-                  <td>
-                    {" "}
-                    <FaTableTennis /> Games room
-                  </td>
+                  <td>Hot tub</td>
+                  <td>Games room</td>
                 </tr>
-                {/* row 4 */}
                 <tr>
-                  <td>
-                    {" "}
-                    <FaBath /> Bath
-                  </td>
-                  <td>
-                    {" "}
-                    <FaWheelchair /> Wheelchair access
-                  </td>
+                  <td>Bath</td>
+                  <td>Wheelchair access</td>
                 </tr>
               </tbody>
             </table>
