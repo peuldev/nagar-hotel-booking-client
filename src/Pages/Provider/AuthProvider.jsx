18f12269a27e712b63e3e3a3eecd6df83f31e62f
peuldev <arfinpeul523@gmail.com>
2023-12-24 23:52:18 +0600

    update

--- src/Pages/Provider/AuthProvider.jsx
+++ src/Pages/Provider/AuthProvider.jsx
@@ -1,9 +1,39 @@
-import { createContext, useState } from "react";
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
+import { createContext, useEffect, useState } from "react";
+import auth from "../Firebase/firebaseConfig";
 
 export const AuthContext = createContext(null);
 const AuthProvider = ({ children }) => {
   const [user, setUser] = useState([]);
-  const AuthInfo = { user };
+  const [loding, setLoding] = useState([]);
+  const createUser = (email, password) => {
+    setLoding(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+  const signIn = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+  const logOut = () => {
+    setLoding(true);
+    return signOut(auth);
+  };
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      console.log("current user", currentUser);
+      setLoding(false);
+    });
+    return () => {
+      return unsubscribe();
+    };
+  }, []);
+  const AuthInfo = { user, loding, createUser, signIn, logOut };
 
   return (
     <AuthContext.Provider value={AuthInfo}>{children}</AuthContext.Provider>
