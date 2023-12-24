dd5a77623b19ab6f4fa1c6de3839dddca1df381f
peuldev <arfinpeul523@gmail.com>
2023-12-24 19:55:30 +0600

    room info

--- src/Pages/Rooms/Rooms.jsx
+++ src/Pages/Rooms/Rooms.jsx
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Rooms = () => {
+  const [featuredRooms, setfeaturedRooms] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:5000/rooms")
+      .then((res) => res.json())
+      .then((data) => setfeaturedRooms(data));
+  }, []);
+  console.log(featuredRooms);
   return (
     <div>
-      <h1>Rooms</h1>
+      <div className="lg:py-20 md:py-10 py-5 text-center space-y-2">
+        <h1 className="lg:text-5xl md:text-3xl text-xl lg:font-bold md:font-semibold font-medium">
+          Choose your Room
+        </h1>
+        <p className="text-base text-[#64688c]">
+          Laborum accusantium libero commodi. Voluptate consequatur itaque
+          expedita accusamus impedit <br /> perspiciatis asperiores
+          necessitatibus assumenda magnam ipsa.
+        </p>
+      </div>
     </div>
   );
 };
