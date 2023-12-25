95774d4e80fa3e99d73dfa0410914dee1064c96a
peuldev <arfinpeul523@gmail.com>
2023-12-25 15:48:55 +0600

    my boking data load

--- src/Pages/Rooms/MyBooking.jsx
+++ src/Pages/Rooms/MyBooking.jsx
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../../Provider/AuthProvider";
 
 const MyBooking = () => {
+  const { user } = useContext(AuthContext);
+  const [myBookings, setmyBookings] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:5000/confirmation")
+      .then((res) => res.json())
+      .then((data) => {
+        setmyBookings(data);
+      });
+  }, []);
+  // const
   return (
     <div>
-      <h1>My Booking</h1>
+      <h1>My Booking{myBookings.length}</h1>
+      {myBookings.map((myBooking) => console.log(myBooking))}
     </div>
   );
 };
