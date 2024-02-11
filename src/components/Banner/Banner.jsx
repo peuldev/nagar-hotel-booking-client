43b9427391280880bb336dfba1433debd22a252e
peuldev <arfinpeul523@gmail.com>
2024-02-12 00:03:25 +0600

    update

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,21 +1,12 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { IoIosSearch } from "react-icons/io";
 import coverImages from "../../assets/Hotel/image-banner.jpg";
 import icon1 from "../../assets/icon/f-1.png";
 import icon2 from "../../assets/icon/f-2.png";
 import icon3 from "../../assets/icon/f-3.png";
 import icon4 from "../../assets/icon/f-4.png";
-import { FaCarAlt } from "react-icons/fa";
-import { FaCar } from "react-icons/fa";
 
 const Banner = () => {
-  const [search, setSearch] = useState("");
-  const handleSearch = (e) => {
-    e.preventDefault();
-    const searchText = e.target.search.value;
-    setSearch(searchText);
-  };
-  console.log(search);
   return (
     <div>
       <div style={{ backgroundImage: "url(" + coverImages + ")" }}>
@@ -29,18 +20,68 @@ const Banner = () => {
               <p className="lg:text-xl md:text-xl lg:py-5 md:py-2 ">
                 Deleniti nostrum laboriosam praesentium quasi quam voluptate.
               </p>
-              {/* form  */}
-              <div>
-                <form onSubmit={handleSearch}>
-                  <input
-                    type="text"
-                    placeholder="Type here"
-                    className="text-[#000] input input-bordered input-primary w-full max-w-xs mr-5"
-                    name="search"
-                  />
+            </div>
+            <div
+              className="bg-white p-3 lg:rounded-full rounded grid lg:grid-flow-col md:grid-cols-2 gap-5 grid-flow-row border-blue lg:border-4 
+                border-opacity-25"
+            >
+              <div className="join">
+                <button className="mr-3 text-base text-[#64688c]">
+                  Check-in
+                </button>
+                <input
+                  className="input bg-[#ecfafb] rounded-lg"
+                  placeholder="Date"
+                  type="date"
+                />
+              </div>
+              <div className="join">
+                <button className="mr-3 text-base text-[#64688c]">
+                  Check-Out
+                </button>
+                <input
+                  className="input  bg-[#ecfafb] rounded-lg"
+                  placeholder="Date"
+                  type="date"
+                />
+              </div>
 
-                  <input type="submit" value="Search" className="btn" />
-                </form>
+              <div className="join">
+                <button className="mr-3 text-base text-[#64688c]">
+                  Adults
+                </button>
+                <select className="select w-full max-w-xs bg-[#ecfafb] rounded-lg">
+                  <option disabled selected>
+                    Pick your Adults
+                  </option>
+                  <option>1</option>
+                  <option>2</option>
+                  <option>3</option>
+                  <option>4</option>
+                  <option>5</option>
+                </select>
+              </div>
+              <div className="join">
+                <button className="mr-3 text-base text-[#64688c]">
+                  Children
+                </button>
+                <select className="select w-full max-w-xs bg-[#ecfafb] rounded-lg">
+                  <option disabled selected>
+                    Pick your Children
+                  </option>
+                  <option>1</option>
+                  <option>2</option>
+                  <option>3</option>
+                  <option>4</option>
+                  <option>5</option>
+                </select>
+              </div>
+              <div className="join">
+                <button className="mr-3 text-base text-[#64688c]">Find</button>{" "}
+                <p className=" bg-blue hover:bg-info text-white rounded-full cursor-pointer text-xl p-4">
+                  {" "}
+                  <IoIosSearch />
+                </p>{" "}
               </div>
             </div>
           </div>
